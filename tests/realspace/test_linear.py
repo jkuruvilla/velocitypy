d5d41f6ae4157c400bf5407574347c2019c381ae
Joseph Kuruvilla <joseph.j.kuruvilla@gmail.com>
2018-08-28 22:50:00 +0200

    Added minimalistic test

---
+++ tests/realspace/test_linear.py
@@ -0,0 +1,11 @@
+import pytest
+import numpy as np
+from velocitypy.realspace.linear import correlation, radial_mean
+
+wavemode_k = np.arange(0.001, 100, 0.01)
+power_spectrum_ones = np.ones(len(wavemode_k))
+power_spectrum_zeros = np.zeros(len(wavemode_k))
+
+
+def test_correlation():
+    assert correlation(1, wavemode_k, power_spectrum_zeros) == 0
