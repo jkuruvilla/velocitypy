0b14da6fe30df3c14bcdd1cf2d8e2f026fcad831
Joseph Kuruvilla <joseph.j.kuruvilla@gmail.com>
2018-08-29 10:54:38 +0200

    fixing the test

--- tests/realspace/test_linear.py
+++ tests/realspace/test_linear.py
@@ -6,6 +6,8 @@ wavemode_k = np.arange(0.001, 100, 0.01)
 power_spectrum_ones = np.ones(len(wavemode_k))
 power_spectrum_zeros = np.zeros(len(wavemode_k))
 
+radial_bins = np.array([1])
+
 
 def test_correlation():
-    assert density_correlation(1, wavemode_k, power_spectrum_zeros) == 0
+    assert density_correlation(radial_bins, wavemode_k, power_spectrum_zeros) == 0
